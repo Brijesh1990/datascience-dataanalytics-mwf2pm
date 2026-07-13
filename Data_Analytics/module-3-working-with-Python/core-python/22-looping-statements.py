144c93139e1958d9b3430ea0878ab5ff042f4e0e
Brijesh1990 <bkpandey.pandey@gmail.com>
2026-07-13 16:06:48 +0530

    13-07-2026 all data uploaded

---
+++ Data_Analytics/module-3-working-with-Python/core-python/22-looping-statements.py
@@ -0,0 +1,159 @@
+# looping statements 
+# loop are used to print number of iteration again and again there we used loop 
+# loop are used to repeat iteration again  and again there we applied loop 
+# there are some types of loop are used in python 
+# 1) for loop 
+# 2) while loop 
+# 3) do while (but not used)
+
+# for loop
+# for loop are executed when condition is true 
+# for are executed to print number of iterations
+
+# syntax 
+
+# for variable in range: 
+#     statements
+    
+# for i in range(0,10):
+#     print(i)    
+
+
+# for i in range(1,5):
+#     print("brijesh",i)
+
+
+# for i in range(1,5):
+#     print("brijesh")    
+   
+
+# w.a.p to iterate brijesh
+# name='brijesh' 
+# for i in name:
+#     print(i)
+
+# name='brijesh' 
+# for i in name:
+#     print(i, end="")
+
+
+# data types list mutable 
+# employee list []
+# emp=["faiz","om","meet","forum","astha","paranav","mishri"]
+# iterate this list 
+# for i in emp:
+#     print(i)
+# print(emp)
+
+
+# number=[1,2,3,4,5,6,7,8,9]
+# print(number)
+# print(type(number))
+
+# iterate this list apply loop
+# for i in number:
+#     # print(i)
+#     # print(i,end="")
+#     # print even numbers from list only 
+#     # if i%2==0:
+#     #     print(i)
+#     # print only odd numbers from list 
+#     if i%2==1:
+#          print(i)
+    
+    
+
+# while loop : while is executed when condition is true 
+# while loop is working to print number of iterations again and again 
+
+# syntax 
+# while(condition):
+    # statements
+    # increment/decrement
+  
+# true  
+# i=0
+# while(i<=10):
+#     print(i)  
+#     i=i+1
+  
+  
+# false
+# i=0
+# while(i>=10):
+#     print(i)  
+#     i=i+1
+
+
+# i=0
+# while(i<=10):
+#     if i==5:
+#         break 
+#     print(i)  
+#     i=i+1
+
+
+# i=0
+# while(i<=10):
+#     if i==5 or i==7 or i==9:
+#         continue
+#     print(i)  
+#     i=i+1
+  
+# infinite loop
+# while True:
+#     print("Hey Brijesh")  
+  
+  
+# i=0
+# while(i<=5):
+#    print("Hey brijesh",i)
+#    i=i+1 
+ 
+
+# infinite loop
+# i=0
+# while True:
+#     print("Hey Brijesh")
+#     i=i+1    
+ 
+  
+  
+# do while loop : do will executed once time either condition true or false  if condition is true while is executed if condition is false nothing will be executed 
+
+    # note : not supported in python 
+ 
+    
+# post increments 
+# i=10
+# i=i+1  #post increment
+# print(i) 
+
+
+# post decrement 
+
+# i=10
+# i=i-1  #post increment
+# print(i)
+ 
+#  H.W 
+# w.a.p to print naturals numbers 
+# w.a.p to print 1 to 10 numbers and find odd numbers only 
+# w.a.p to print 1 to 10 numbers and find even numbers only
+# w.a.p to find infinite loop 
+# w.a.p to find factorials numbers 
+# w.a.p to find armstrong numbers 
+# w.a.p to find a numbers is prime or not 
+
+# w.a.p to find choice based calculator choice a numbers 
+# 1=additions
+# 2=multiplications
+# 3=substraction
+# 4=divisions
+# 5=exit   
+
+# create a list and iterate via for loop
+# create a dictionary and iterate via for loop
+# create a tuple and iterate via for loop
+ 
+     
---
+++ Data_Analytics/module-3-working-with-Python/core-python/24-control-flow-statements.py
@@ -0,0 +1,24 @@
+# 3) Control-flow statements:
+#    - break
+#    - continue
+#    - return
+#    - raise
+#    - yield
+
+
+#  break : is a control flow statements it is used for control loop and terminate or break the execution of loop
+ 
+# for i in range(1,100):
+#     # print(i)
+#     # control flow statements 
+#     if i==21:
+#         break
+#     print(i)
+
+# continue : is a control flow statements it is used o control loop and skip one iteration and executed continue the loop
+for i in range(1,100):
+    # print(i)
+    # control flow statements 
+    if i==21 or i==31 or i==41:
+        continue
+    print(i)
