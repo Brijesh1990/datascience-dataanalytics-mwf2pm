233271d6b4f2e7bc7af209ccf96433a8a538b200
Brijesh1990 <bkpandey.pandey@gmail.com>
2026-08-07 16:33:47 +0530

    07-08-2026 all data uploaded

--- Data_Analytics/module-3-working-with-Python/advanced-python/python-statements/2-single line statements.py
+++ Data_Analytics/module-3-working-with-Python/advanced-python/python-statements/2-single line statements.py
@@ -8,7 +8,20 @@
 # name=input("Enter a Name :")
 # print(name)
 
-name=input("Enter a Name :")
-if name=='brijesh':
-    pass 
-    print(name)
+# name=input("Enter a Name :")
+# if name=='brijesh':
+#     pass 
+#     print(name)
+
+# compare 
+
+# a=10
+# b=20
+# print(a==b) 
+
+
+# a=20
+# b=20
+# print(a==b) 
+
+
